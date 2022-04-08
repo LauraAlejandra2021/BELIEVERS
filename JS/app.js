7a048b36767fbd8bd13afa70de8dcd5bd57a6ab8
LauraAlejandra2021 <3450@holbertonschool.com>
2022-04-08 09:59:34 -0500

    structure

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@ addEventListener('DOMContentloaded', () => {
     const btn_menu = document.querySelector('.btn_menu')
     if (btn_menu) {
         btn_menu.addEventListener('click', () =>{
-             const menu = document.querySelector('.menu_items')
+             const menu = document.querySelector('.menu')
              menu.classList.toggle('show')
         })
     }
