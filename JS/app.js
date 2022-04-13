b367e206ee9d64d9e0a286a6032b03ec5202b9da
LauraAlejandra2021 <3450@holbertonschool.com>
2022-04-13 11:50:40 -0500

    responsive

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,3 @@
-addEventListener('DOMContentloaded', () => {
-    const btn_menu = document.querySelector('.btn_menu')
-    if (btn_menu) {
-        btn_menu.addEventListener('click', () =>{
-             const menu = document.querySelector('.menu')
-             menu.classList.toggle('show')
-        })
-    }
-})
+function showmenu() {
+    document.getElementById("menu").classList.toggle("show");
+  }
